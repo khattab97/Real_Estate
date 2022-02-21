87176e2ff18295f931e2c9fdb4212e680864caca
Milk Steak <a.khatttab97@gmail.com>
2022-02-21 17:37:36 +0200

    Adding pages app to urls and settings

--- BTRE_Project/settings.py
+++ BTRE_Project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,6 +56,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [BASE_DIR / 'templates']
+        #'DIRS': [os.path.join(BASE_DIR, 'templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {

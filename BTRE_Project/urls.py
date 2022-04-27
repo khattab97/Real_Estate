3460f265625d1d89b7442a23966fc593568a127e
Milk Steak <a.khatttab97@gmail.com>
2022-04-27 14:37:58 +0200

    Adding / after listing route

--- BTRE_Project/urls.py
+++ BTRE_Project/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('pages.urls')),
-    path('listing', include('listings.urls')),
+    path('listing/', include('listings.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 

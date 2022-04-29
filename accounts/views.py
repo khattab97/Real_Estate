b1621d4795df3b1e59fa7113f51c607eb4fed7af
Milk Steak <a.khatttab97@gmail.com>
2022-04-29 21:20:26 +0200

    Create Accounts app

---
+++ accounts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect
+
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+def logout(request):
+    return  redirect('pages:index')
+
+def register(request):
+    return render(request, 'accounts/register.html')
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')

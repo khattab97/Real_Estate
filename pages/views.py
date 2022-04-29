93a8748dcd2eafbf6f637b22bbd0909000bb614e
Milk Steak <a.khatttab97@gmail.com>
2022-04-29 11:05:18 +0200

    Adding listing to index page and realtors to about page

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
+from listings.models import Listing
+from realtors.models import Realtor
 from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    ctx = {'listings': listings}
+    return render(request, 'pages/index.html', ctx)
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    realtors = Realtor.objects.order_by('-hire_date')
+    mvp_realtor = Realtor.objects.all().filter(is_mvp=True)
+    ctx = {
+        'realtors': realtors,
+        'mvp_realtor': mvp_realtor
+    }
+    return render(request, 'pages/about.html', ctx)

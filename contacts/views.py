42eaeada103085002c4c4aefbd7020456a70586e
Milk Steak <a.khatttab97@gmail.com>
2022-05-02 17:50:36 +0200

    Adding sending email functionality

--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Contact
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
 
@@ -15,12 +17,24 @@ def contact(request):
         phone = request.POST['phone']
         message = request.POST['message']
 
+        if user_id:
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                messages.error(request, 'You have already made an inquiry for this listing')
+                return redirect('/listing/' + listing_id)
+
         contact1 = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
         contact1.save()
+        send_mail(
+            'Property Listing Inquiry',
+            'There has been an inquiry for ' + listing + '. Sign into admin panel for more info.',
+            settings.EMAIL_HOST_USER,
+            [realtor_email, 'a.khatttab97@gmail.com'],
+            fail_silently=False
+        )
         messages.success(request, 'Your request has been submitted, A realtor will get back to you soon')
         return redirect('/listing/'+listing_id)
 
 
 
 
-    return

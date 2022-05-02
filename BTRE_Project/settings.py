e6856f38719d1d841690fd2f8c00f105ddb653b7
Milk Steak <a.khatttab97@gmail.com>
2022-05-02 17:50:00 +0200

    Adding email settings

--- BTRE_Project/settings.py
+++ BTRE_Project/settings.py
@@ -143,3 +143,11 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.mail.yahoo.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
